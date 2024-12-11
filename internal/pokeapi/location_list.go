971fbeba9ece2057fdaff6b460cbecd7bbe4f9e8
4madness7 <marcomirizzi4@gmail.com>
2024-12-11 15:56:08 +0100

    change default endpoint for map navigation

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (c *Client) ListLocations(pageURL *string, cache *pokecache.Cache) (DataLocationArea, error) {
-	url := baseURL + "/location-area"
+	url := baseURL + "/location-area?offset=0&limit=20"
 	if pageURL != nil {
 		url = *pageURL
 	}
